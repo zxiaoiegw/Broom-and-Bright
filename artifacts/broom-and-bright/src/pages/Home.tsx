c115ca6e41b573ce84ce722cfbee8d6ba9c211c7
Zhen Xiao <kittyiegw@gmail.com>
2026-08-16 10:28:41 -0500

    SEO optimize

--- artifacts/broom-and-bright/src/pages/Home.tsx
+++ artifacts/broom-and-bright/src/pages/Home.tsx
@@ -9,8 +9,14 @@ import { FAQ } from '@/components/sections/FAQ';
 import { ContactForm } from '@/components/sections/ContactForm';
 import { Footer } from '@/components/sections/Footer';
 import { MobileStickyBooking } from '@/components/MobileStickyBooking';
+import { useDocumentHead } from '@/hooks/useDocumentHead';
 
 export default function Home() {
+  useDocumentHead(
+    'Broom & Bright | Professional Cleaning Services in Kansas City',
+    'Professional, trusted, and thorough residential cleaning services in the Kansas City area.',
+  );
+
   return (
     <div className="min-h-screen flex flex-col bg-white overflow-x-hidden font-sans">
       <Nav />
